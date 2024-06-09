431491412ecbba63258689823b6ae6970518f3e7
dhiego <dhiego.silva@hotmail.com>
2024-06-09 15:10:00 +0200

    rest api with strava =D

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,10 +8,23 @@ import { Observable } from 'rxjs';
 export class ApiService {
 
   private apiUrl = 'https://jsonplaceholder.typicode.com/posts'; // Replace with your API URL
+  private apiUrl2 = 'https://www.strava.com/api/v3/clubs/978870/activities?page=1&per_page=200';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getPosts(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
+
+  getData(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer a534291ae6f38a2eab9c95723de1919d60fa6f26'
+    });
+
+    return this.http.get<any>(`${this.apiUrl2}`, {headers});
+  }
 }
+
+
